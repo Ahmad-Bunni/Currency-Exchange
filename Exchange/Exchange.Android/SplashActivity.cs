3e4830832945a3ffc83d4eb392756aa13dc22073
Ahmad Bunni <ahmad-bunni@hotmail.com>
2019-04-14 21:01:59 +0100

    Add project files.

---
+++ Exchange/Exchange.Android/SplashActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Support.V7.App;
+using System.Threading.Tasks;
+
+namespace Exchange.Droid
+{
+    [Activity(Theme = "@style/MainTheme.Splash", MainLauncher = true, NoHistory = true)]
+    public class SplashActivity : AppCompatActivity
+    {
+        public override void OnCreate(Bundle savedInstanceState, PersistableBundle persistentState)
+        {
+            base.OnCreate(savedInstanceState, persistentState);
+
+        }
+
+        // Launches the startup task
+        protected override void OnResume()
+        {
+            base.OnResume();
+            Task startupWork = new Task(() => { SimulateStartup(); });
+            startupWork.Start();
+        }
+
+        public override void OnBackPressed() { }
+
+        // Simulates background work that happens behind the splash screen
+        void SimulateStartup()
+        {
+            StartActivity(new Intent(Application.Context, typeof(MainActivity)));
+        }
+    }
+}
